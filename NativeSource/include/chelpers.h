d66cd6186c90638b41d99f85569f166647f5b7e3
fukuroda <fukuroder@live.jp>
2015-12-05 01:37:22 +0900

    Create blit_saw project and blit_square project

---
+++ NativeSource/include/chelpers.h
@@ -0,0 +1,39 @@
+/**
+ *  C Helper structs and functions to build dsp scripts.
+ *  Copyright (c) 2015 Blue Cat Audio. All rights reserved.
+ */
+
+#ifndef _chelpers_h
+#define _chelpers_h
+
+#include "dspapi.h"
+
+/** C Array Descriptor expected by host for arrays of strings.
+ *  (inputParametersNames, inputParametersUnits, etc.)
+ */
+struct CStringArray
+{
+    char const** ptr;
+    uint         length;
+};
+
+
+/** C Array Descriptor expected by host for arrays of double values.
+ *  (inputParameters, outputParameters etc.)
+ */
+struct CDoubleArray
+{
+    double*     ptr;
+    uint        length;
+};
+
+/** C Array Descriptor expected by host for arrays of integer values.
+ *  (inputParametersSteps)
+ */
+struct CIntArray
+{
+    int*        ptr;
+    uint        length;
+};
+
+#endif
---
+++ NativeSource/src/samples/library/Constants.h
@@ -0,0 +1,11 @@
+/** 
+ *  \file Constants.hxx
+ *  Common math constants for dsp scripting.
+ * 
+ *  Created by Blue Cat Audio <services@bluecataudio.com>
+ *  Copyright 2014 Blue Cat Audio. All rights reserved.
+ *
+ */
+
+/// Pi value
+const double PI=3.141592653589793238462;
